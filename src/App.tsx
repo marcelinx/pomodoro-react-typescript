d4c95b27a04af7aa168c89a8d481261968f201c0
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-07-20 13:10:54 -0300

    setting css

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,13 @@ import { PomodoroTimer } from './components/pomodoro-timer';
 
 function App(): JSX.Element {
   return (
-  <div className="app">
-    <PomodoroTimer defaultPomodoroTime={60}/>
+  <div className="container">
+    <PomodoroTimer
+    PomodoroTime={1500}
+    shortRestTime={300}
+    longRestTime={900}
+    cycles={4}
+    />
   </div>
   );
 }
